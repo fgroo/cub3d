517664668d02436ee631f2c2ac558877e6d43f17
fgroo <faris.goerlich@gmail.com>
2026-02-26 20:51:49 +0100

    v1 of parser01 - untested

--- src/parsing/parser01.c
+++ src/parsing/parser01.c
@@ -6,63 +6,94 @@
 /*   By: fgroo <student@42.eu>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 19:41:45 by fgroo             #+#    #+#             */
-/*   Updated: 2026/02/26 16:27:17 by fgroo            ###   ########.fr       */
+/*   Updated: 2026/02/26 20:48:37 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // return (printf("Error\n"), 1);
 // return (printf("Error\nWrong Format\n"), 1);
 
-
 #include "parser.h"
-
 #include "error.h"
 #include "libft.h"
 
 #include <stdio.h>
 #include <fcntl.h>
 
-int validate_format(int fd, t_mapdata *mapdata)
+static int	check_texture_file(char *tex, char *line, char *identifier)
 {
-	static char format[7][4] = {"NO ", "SO ", "WE ", "EA ", "F ", "C ", {0}};
-	char		*tmp;
-	int			i;
-
-	i = 0;
-	while (i < 4)
-	{
-		tmp = get_next_line(fd);
-		if (!tmp)
-			pr_error("GNL\n");
-		if (ft_strncmp(tmp, format[i], 3))
-			pr_error("Wrong Format\n");
-		mapdata->tex[i++] = tmp;
-	}
-	tmp = get_next_line(fd);
-
+	tex = fetch_texture_file(line, identifier);
+	if (!tex && (free(line), 1))
+		return (pr_error("failed fetching texture file\n"), 1);
 	return (0);
 }
 
-char **hardcoded(char ***argv)
+static int	check_fetch_color(t_mapdata *mapdata, char *line,
+	char *identifier, int i)
 {
-	char **hardcode;
-	int	i;
+	if (i == 5)
+	{
+		mapdata->floor_color = fetch_color(line, identifier);
+		if (mapdata->floor_color == -1)
+			return (pr_error("failed fetching color"), 1);
+	}
+	else if (i == 6)
+	{
+		mapdata->ceiling_color = fetch_color(line, identifier);
+		if (mapdata->ceiling_color == -1)
+			return (pr_error("failed fetching color"), 1);
+	}
+	return (0);
+}
+
+int	validate_format(int fd, t_mapdata *mapdata)
+{
+	static char	format[7][3] = {"NO", "SO", "WE", "EA", "F", "C", {0}};
+	char		*tmp;
+	int			i;
+
+	i = -1;
+	while (++i < 6)
+	{
+		tmp = get_next_line(fd);
+		if (!tmp)
+			return (pr_error("GNL\n"), 1);
+		if (tmp[0] == '\n' && (free(tmp), 1))
+			continue ;
+		if (ft_strncmp(tmp, format[i], ft_strlen(format[i])) && (free(tmp), 1))
+			return (pr_error("Wrong Format\n"), 1);
+		if (i < 4 && check_texture_file(mapdata->tex[i], tmp, format[i]))
+			return (1);
+		else if (i >= 4 && check_fetch_color(mapdata, tmp, format[i], i))
+			return (1);
+	}
+	return (0);
+}
+
+char	**hardcoded(char ***argv)
+{
+	char		**hardcode;
+	static char	arg[] = "./maps/example.cub";
+	int			i;
 
 	i = 0;
 	while (*argv[i])
-		argv[i++] = NULL;
+		*argv[i++] = NULL;
 	hardcode = malloc(sizeof(char *) * 2);
 	if (!hardcode)
 		return (pr_error("malloc\n"), NULL);
-	hardcode[0] = "../maps/example.cub";
+	hardcode[0] = malloc(sizeof(ft_strlen(arg)) + 1);
+	if (!hardcode[0])
+		return (pr_error("malloc\n"), NULL);
+	hardcode[0] = arg;
 	hardcode[1] = NULL;
 	return (hardcode);
 }
 
-int parser(t_mapdata *map, char *argv[])
+int	parser(t_mapdata *map, char *argv[])
 {
-	int fd;
-	
+	int	fd;
+
 	if (!map)
 		return (pr_error(""), 1);
 	argv = hardcoded(&argv);
@@ -75,7 +106,3 @@ int parser(t_mapdata *map, char *argv[])
 		return (1);
 	return (0);
 }
-
-
-// TODO: 
-// 1. 
