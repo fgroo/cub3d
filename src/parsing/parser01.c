017e4eea79bebe7abeac68c5776a2fd213f9d262
fgroo <faris.goerlich@gmail.com>
2026-03-07 21:05:37 +0100

    rotation and moving works; code bloated with tests right now
    rebase

--- src/parsing/parser01.c
+++ src/parsing/parser01.c
@@ -6,7 +6,7 @@
 /*   By: fgroo <student@42.eu>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 19:41:45 by fgroo             #+#    #+#             */
-/*   Updated: 2026/03/05 15:15:28 by fgroo            ###   ########.fr       */
+/*   Updated: 2026/03/07 19:35:39 by fgroo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,7 +89,6 @@ int	parser(t_mapdata *map, char *file)
 	if (flood_map(map))
 		return (free_mapdata(map), close(fd), 1);
 	print_mapdata(map);
-	free_mapdata(map);
 	close(fd);
 	return (0);
 }
