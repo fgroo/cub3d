bab74386014ee09dd45f4452ea9c9915d41795cd
9S <robeats.hale@gmail.com>
2026-03-12 02:09:08 +0100

    adjusted cleanup for reworked image struct

--- src/cleanup/free_images.c
+++ src/cleanup/free_images.c
@@ -6,7 +6,7 @@
 /*   By: rtwobie <student@42>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 17:41:46 by rtwobie           #+#    #+#             */
-/*   Updated: 2026/03/09 17:44:54 by rtwobie          ###   ########.fr       */
+/*   Updated: 2026/03/11 18:18:02 by rtwobie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 
 void	free_images(mlx_t *mlx, t_image *image)
 {
-	if (image->player)
-		mlx_delete_image(mlx, image->player);
+	if (image->map_buf)
+		mlx_delete_image(mlx, image->map_buf);
 	if (image->map)
 		mlx_delete_image(mlx, image->map);
 	free(image);
